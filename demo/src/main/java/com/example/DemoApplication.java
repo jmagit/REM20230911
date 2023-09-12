6133c70b96e51b33041bd79a61a58d15bd867fb7
Javier <jmartin@grupoloyal.com>
2023-09-12 12:56:02 +0200

    Consultas

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +15,29 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicación arrancada cambio");
+		
+//		dao.save(new Actor(0, "Pepito", "Grillo"));
+//		var item = dao.findById(202);
+//		if(item.isEmpty()) {
+//			System.err.println("No encontrado");
+//		} else {
+//			var actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);
+//		}
+//		dao.deleteById(202);
+//		dao.findAll().forEach(System.out::println);
+//		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.out::println);
+//		dao.findByActorIdGreaterThan(200).forEach(System.out::println);
+//		dao.findUltimosJPQL(200).forEach(System.out::println);
+		dao.findUltimosSQL(200).forEach(System.out::println);
+		dao.findAll((root, query, builder) -> builder.greaterThan(root.get("actorId"), 200)).forEach(System.out::println);
 	}
 
 }
---
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -0,0 +1,20 @@
+package com.example.domains.contracts.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.domains.entities.Actor;
+
+public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findByActorIdGreaterThan(int id);
+	
+	@Query("from Actor a where a.actorId > ?1")
+	List<Actor> findUltimosJPQL(int id);
+	@Query(value = "select * from actor a where a.actor_id > ?1", nativeQuery = true)
+	List<Actor> findUltimosSQL(int id);
+	
+}
