079665ce1a3f0bcf9bdec011b4bc3c5ca904a688
Javier <jmartin@grupoloyal.com>
2023-09-13 09:21:10 +0200

    Validaciones

--- demo/src/main/java/com/example/domains/entities/Actor.java
+++ demo/src/main/java/com/example/domains/entities/Actor.java
@@ -2,6 +2,11 @@ package com.example.domains.entities;
 
 import java.io.Serializable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Objects;
@@ -23,12 +28,18 @@ public class Actor implements Serializable {
 	private int actorId;
 
 	@Column(name="first_name", nullable=false, length=45)
+	@NotBlank
+	@Size(max = 45, min=2)
+	@Pattern(regexp = "^[A-Z]+$")
 	private String firstName;
 
 	@Column(name="last_name", nullable=false, length=45)
+	@NotBlank
+	@Size(max = 45)
 	private String lastName;
 
 	@Column(name="last_update", insertable=false, updatable=false, nullable=false)
+	@PastOrPresent
 	private Timestamp lastUpdate;
 
 	//bi-directional many-to-one association to FilmActor
